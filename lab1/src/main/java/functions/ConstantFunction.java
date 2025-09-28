de002c9799da9561e73c1884654aa772c50db847
EvgenyJ7 <tigersamara@gmail.com>
2025-09-28 19:29:27 +0400

    Add ConstantFunction

---
+++ lab1/src/main/java/functions/ConstantFunction.java
@@ -0,0 +1,14 @@
+package functions;
+
+public class ConstantFunction implements MathFunction {
+    private final double constant;
+
+    public ConstantFunction(double constant) {
+        this.constant = constant;
+    }
+
+    @Override
+    public double apply(double x) {
+        return constant;
+    }
+}
