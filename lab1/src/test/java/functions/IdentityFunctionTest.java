f64830096d6650641e99299ccfe5def8b0528818
Maksim Novikov <nowikoff402@gmail.com>
2025-09-30 18:18:49 +0200

    Дополнены тесты к  IdentityFunction

--- lab1/src/test/java/functions/IdentityFunctionTest.java
+++ lab1/src/test/java/functions/IdentityFunctionTest.java
@@ -12,4 +12,21 @@ class IdentityFunctionTest {
         assertEquals(0.0, function.apply(0.0), 0.0001);
         assertEquals(-3.5, function.apply(-3.5), 0.0001);
     }
+    @Test
+    void testApplyWithZero() {
+        IdentityFunction function = new IdentityFunction();
+        //  f(0) = 0
+        assertEquals(0.0, function.apply(0.0), 1e-10);
+    }
+    @Test
+    void testApplyWithDecimalNumbers() {
+        IdentityFunction function = new IdentityFunction();
+        // Проверяем работу с дробными числами
+        assertEquals(2.5, function.apply(2.5), 1e-10);
+        assertEquals(0.333333, function.apply(0.333333), 1e-10);
+        assertEquals(-7.89, function.apply(-7.89), 1e-10);
+    }
+
+
 }
+
