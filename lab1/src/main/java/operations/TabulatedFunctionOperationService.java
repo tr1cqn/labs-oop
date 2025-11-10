08d50b358b5f117ed3bfde636484e0a63b45754f
EvgenyJ7 <tigersamara@gmail.com>
2025-11-10 21:14:06 +0400

    Создан InconsistentFunctionsException и обновлён TabulatedFunctionOperationService

--- lab1/src/main/java/operations/TabulatedFunctionOperationService.java
+++ lab1/src/main/java/operations/TabulatedFunctionOperationService.java
@@ -2,21 +2,87 @@ package operations;
 
 import functions.Point;
 import functions.TabulatedFunction;
+import functions.factory.TabulatedFunctionFactory;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import exceptions.InconsistentFunctionsException;
 
 public class TabulatedFunctionOperationService {
 
+    private TabulatedFunctionFactory factory;
 
+    // Конструкторы
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    // Геттер и сеттер для фабрики
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    // Вложенный интерфейс для бинарных операций
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    // Существующий метод asPoints
     public static Point[] asPoints(TabulatedFunction function) {
         int pointCount = function.getCount();
         Point[] points = new Point[pointCount];
 
         int i = 0;
-        // Используем цикл for-each для обхода всех точек функции
         for (Point point : function) {
             points[i] = point;
             i++;
         }
 
         return points;
+    }
+
+    // Приватный метод для выполнения операций
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        // Проверяем одинаковое количество точек
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("Функции имеют разное количество точек");
+        }
+
+        // Получаем точки обеих функций
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+
+        int pointCount = pointsA.length;
+        double[] xValues = new double[pointCount];
+        double[] yValues = new double[pointCount];
+
+        // Обрабатываем каждую точку
+        for (int i = 0; i < pointCount; i++) {
+            // Проверяем совпадение x-координат
+            if (Math.abs(pointsA[i].x - pointsB[i].x) > 1e-10) {
+                throw new InconsistentFunctionsException("X-координаты функций не совпадают");
+            }
+
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        // Создаем новую функцию через фабрику
+        return factory.create(xValues, yValues);
+    }
+
+    // Публичные методы операций
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
     }
 }
