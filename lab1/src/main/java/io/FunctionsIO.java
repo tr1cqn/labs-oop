c14d5487ede2cd89de4b57376455cc283e3d0117
EvgenyJ7 <tigersamara@gmail.com>
2025-11-12 19:02:29 +0400

    Создан пакет io и в нём завершённый класс FunctionsIO

---
+++ lab1/src/main/java/io/FunctionsIO.java
@@ -0,0 +1,9 @@
+package io;
+
+public final class FunctionsIO {
+
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Нельзя создавать экземпляры утилитного класса FunctionsIO");
+    }
+
+}
