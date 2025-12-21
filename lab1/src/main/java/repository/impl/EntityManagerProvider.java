3ec1563bd8949fd4ec16449de851e566999c3bd9
EvgenyJ7 <tigersamara@gmail.com>
2025-12-21 14:11:44 +0400

    Небольшие изменения

--- lab1/src/main/java/repository/impl/EntityManagerProvider.java
+++ lab1/src/main/java/repository/impl/EntityManagerProvider.java
@@ -1,39 +1,59 @@
 package repository.impl;
 
+import entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Properties;
 
 /**
  * Провайдер EntityManager для работы с Hibernate
  */
 public class EntityManagerProvider {
-    private static EntityManagerFactory emf;
-    private static final String PERSISTENCE_UNIT_NAME = "lab5-pu";
+    private static SessionFactory sessionFactory;
     
     static {
-        Map<String, String> properties = new HashMap<>();
-        properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-        properties.put("javax.persistence.jdbc.url", "jdbc:postgresql://localhost:5432/lab5_db");
-        properties.put("javax.persistence.jdbc.user", "postgres");
-        properties.put("javax.persistence.jdbc.password", "postgres");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        properties.put("hibernate.hbm2ddl.auto", "update");
-        properties.put("hibernate.show_sql", "false");
-        
-        emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME, properties);
+        try {
+            Configuration configuration = new Configuration();
+            
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "org.postgresql.Driver");
+            settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/lab5_db");
+            settings.put(Environment.USER, "postgres");
+            settings.put(Environment.PASS, "postgres");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+            settings.put(Environment.HBM2DDL_AUTO, "update");
+            settings.put(Environment.SHOW_SQL, "false");
+            
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Function.class);
+            configuration.addAnnotatedClass(Point.class);
+            configuration.addAnnotatedClass(Result.class);
+            
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            System.err.println("Ошибка при инициализации Hibernate: " + e.getMessage());
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
     }
     
     public static EntityManager getEntityManager() {
-        return emf.createEntityManager();
+        return sessionFactory.createEntityManager();
     }
     
     public static void close() {
-        if (emf != null && emf.isOpen()) {
-            emf.close();
+        if (sessionFactory != null && !sessionFactory.isClosed()) {
+            sessionFactory.close();
         }
     }
 }
-
