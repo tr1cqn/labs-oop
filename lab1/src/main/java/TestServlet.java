c824a32542761060c38a51ccd243ced536e4813e
EvgenyJ7 <tigersamara@gmail.com>
2025-12-22 02:03:48 +0400

    Apache Tomcat скачан и подключен к проекту

---
+++ lab1/src/main/java/TestServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/api/v1/test")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        String jsonResponse = "{\"success\": true, \"data\": \"Tomcat работает!\", \"message\": \"API работает\"}";
+        out.print(jsonResponse);
+        out.flush();
+    }
+}
